d28f00b80d27711227a6d35faf9912693a1cfe78
Pambudi Satria <udi_blackf@yahoo.com>
2016-10-04 15:02:16 +0700

    Remove all
    Remove all

--- hr_salary_add/models/__init__.py
+++
@@ -1,8 +0,0 @@
-import hr_salary_particular
-import hr_insentif
-import hr_overtime
-import delivery_package
-import hr_salary_proposal
-import hr_employee_mutation
-import hr_memorandum
-import hr_promotion
