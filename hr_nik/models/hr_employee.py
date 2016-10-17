2385a89ebc706a2671b53792d89d36be50fafbd9
timotiuswigianto <timotius@sicepat.com>
2016-10-17 16:08:43 +0700

    v2

---
+++ hr_nik/models/hr_employee.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+# © 2011, 2013 Michael Telahun Makonnen <mmakonnen@gmail.com>
+# © 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+import random
+import string
+from openerp import models, fields, api, _
+from openerp.exceptions import Warning as UserWarning
+
+
+class HrEmployee(models.Model):
+    """Implement company wide unique identification number."""
+
+    _inherit = 'hr.employee'
+
+    nik = fields.Char(
+        string='NIK',
+        copy=False,
+        readonly=True
+    )
+
+    _sql_constraints = [
+        ('nik_uniq', 'unique(nik)',
+         'The Employee Number must be unique across the company(s).'),
+    ]
+
+    @api.model
+    def _generate_nik(self):
+        """Generate a random employee identification number"""
+        company = self.env.user.company_id
+        employee_id = False
+        if company.employee_id_gen_method == 'sequence':
+            employee_id = self.env['ir.sequence'].get_id(
+                company.employee_id_sequence.id)
+        elif company.employee_id_gen_method == 'random':
+            employee_id_random_digits = company.employee_id_random_digits
+            tries = 0
+            max_tries = 50
+            while tries < max_tries:
+                rnd = random.SystemRandom()
+                employee_id = ''.join(rnd.choice(string.digits)
+                                      for _ in
+                                      xrange(employee_id_random_digits))
+                if not self.search_count([('nik',
+                                           '=',
+                                           employee_id)]):
+                    break
+                tries += 1
+            if tries == max_tries:
+                raise UserWarning(_('Unable to generate an Employee ID number that \
+                is unique.'))
+        return employee_id
+
+    @api.model
+    def create(self, vals):
+        eid = self._generate_nik()
+        vals['nik'] = eid
+        return super(HrEmployee, self).create(vals)
