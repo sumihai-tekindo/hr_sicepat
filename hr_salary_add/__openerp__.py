9448bc1d2455baed6edd7cb1138939c620263328
Pambudi Satria <udi_blackf@yahoo.com>
2016-10-19 13:49:25 +0700

    Replace object for Komponen Gaji

--- hr_salary_add/__openerp__.py
+++ hr_salary_add/__openerp__.py
@@ -1,38 +1,60 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "hr_salary_add",
+    'name': "HR Sicepat Ekspres",
 
-    'summary': "Courses, Sessions, Subscriptions",
+    'summary': "HR for Sicepat Ekspres Indonesia",
 
-    'description': "...",
+    'description': """
+==================
+HR Sicepat Ekspres
+==================
 
-    'author': "Your Company",
-    'website': "http://www.yourcompany.com",
+
+
+    """,
+
+    'author': 'Timotius Wigianto, '
+              'Pambudi Satria',
+    'website': "https://github.com/timotiuswigianto/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Tools',
-    'version': '1.0',
+    'category': 'Human Resources',
+    'version': '8.0.0.1.0',
 
     # any module necessary for this one to work correctly
-    'depends': ['account', 'analytic', 'hr_payroll'],
+    'depends': [
+        'account',
+        'analytic',
+        'hr_payroll',
+    ],
 
     # always loaded
-    'data': ["views/hr_salary_particular_view.xml","views/hr_delivery_package_view.xml","views/hr_overtime_view.xml"
-             ,"views/hr_insentif_view.xml","views/hr_employee_mutation_view.xml","views/hr_memorandum_view.xml",
-             "views/hr_promotion_view.xml","views/hr_salary_proposal_view.xml","views/hr_loan_view.xml",
-             "data/hr_loan_sequence.xml",
-             "data/delivery_package_sequence.xml",
-             "data/hr_employee_mutation_sequence.xml",
-             "data/hr_insentif_sequence.xml",
-             "data/hr_memorandum_sequence.xml",
-             "data/hr_overtime_sequence.xml",
-             "data/hr_promotion_sequence.xml",
-             "data/hr_salary_particular_sequence.xml",
-             "data/hr_salary_proposal_sequence.xml"],
+    'data': [
+        "views/hr_salary_structure_amt_view.xml",
+        "views/hr_delivery_package_view.xml",
+        "views/hr_overtime_view.xml",
+        "views/hr_insentif_view.xml",
+        "views/hr_employee_mutation_view.xml",
+        "views/hr_memorandum_view.xml",
+        "views/hr_promotion_view.xml",
+        "views/hr_salary_proposal_view.xml",
+        "views/hr_loan_view.xml",
+        "data/hr_salary_structure_amt_seq.xml",
+        "data/hr_loan_sequence.xml",
+        "data/delivery_package_sequence.xml",
+        "data/hr_employee_mutation_sequence.xml",
+        "data/hr_insentif_sequence.xml",
+        "data/hr_memorandum_sequence.xml",
+        "data/hr_overtime_sequence.xml",
+        "data/hr_promotion_sequence.xml",
+        "data/hr_salary_proposal_sequence.xml"],
     # only loaded in demonstration mode
     'demo': [],
     'images': [],
+    'qweb': [],
+    'installable': True,
+    'auto_install': False,
     'application': True,
 }
