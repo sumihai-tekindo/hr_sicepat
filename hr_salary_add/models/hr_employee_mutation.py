d6b5fd764307eecfefb309b99bbc3cefe9ca63e9
timotiuswigianto <timotius@sicepat.com>
2016-10-26 17:56:29 +0700

    usulan jabatan mutasi karyawan

--- hr_salary_add/models/hr_employee_mutation.py
+++ hr_salary_add/models/hr_employee_mutation.py
@@ -9,8 +9,8 @@ class hr_employee_mutation(models.Model):
     requestor = fields.Many2one('res.users', string="Requestor", default=lambda self: self.env.user)
     
     nama_karyawan = fields.Many2one("hr.employee", string="Nama Karyawan", required=True)
-    cabang_awal = fields.Many2one('account.analytic.account', string="Cabang Asal", required=True)
-    cabang_baru = fields.Many2one('account.analytic.account', string="Cabang Baru", required=True)
+    cabang_awal = fields.Many2one('hr.department', string="Cabang Asal", readonly=True)
+    cabang_baru = fields.Many2one('hr.department', string="Cabang Baru", required=True)
     alasan = fields.Text()
     
     state = fields.Selection([
@@ -26,8 +26,30 @@ class hr_employee_mutation(models.Model):
 
     @api.multi
     def action_approve(self):
+        self.ensure_one()
         self.state = 'approved'
+        employee = self.env['hr.employee'].search([('id','=',self.nama_karyawan.id)])
+        employee.write({'department_id':self.cabang_baru.id})
         
     @api.multi
     def action_reject(self):
         self.state = 'reject'
+        
+    @api.onchange('nama_karyawan')
+    def onchange_cabang_asal(self):
+        if(self.nama_karyawan):
+            employee = self.env['hr.employee'].search([('id','=',self.nama_karyawan.id)])
+            self.cabang_awal = employee.department_id.id
+            
+    @api.model
+    def create(self, vals):
+        employee = self.env['hr.employee'].search([('id','=',vals['nama_karyawan'])])
+        vals['cabang_awal'] = employee.department_id.id
+        return super(hr_employee_mutation, self).create(vals)
+    
+    @api.multi
+    def write(self, vals):
+        employee = self.env['hr.employee'].search([('id','=',vals.get('nama_karyawan'))])
+        if(employee):
+            vals['cabang_awal'] = employee.department_id.id
+        return super(hr_employee_mutation, self).write(vals)
--- hr_salary_add/models/hr_promotion.py
+++ hr_salary_add/models/hr_promotion.py
@@ -9,10 +9,10 @@ class hr_promotion(models.Model):
     requestor = fields.Many2one('res.users', string="Requestor", default=lambda self: self.env.user)
     
     nama_karyawan = fields.Many2one("hr.employee", string="Nama Karyawan", required=True)
-    jabatan_awal = fields.Many2one('hr.job', string="Jabatan Semula", required=True)
-    cabang_awal = fields.Many2one('account.analytic.account', string="Cabang Asal", required=True)
+    jabatan_awal = fields.Many2one('hr.job', string="Jabatan Semula", readonly=True)
+    cabang_awal = fields.Many2one('hr.department', string="Cabang Asal", readonly=True)
     usulan_jabatan_baru = fields.Many2one('hr.job', string="Usulan Jabatan", required=True)
-    cabang_baru = fields.Many2one('account.analytic.account', string="Cabang Baru", required=True)
+    cabang_baru = fields.Many2one('hr.department', string="Cabang Baru", required=True)
     
     state = fields.Selection([
             ('open','Open'),
@@ -28,7 +28,32 @@ class hr_promotion(models.Model):
     @api.multi
     def action_approve(self):
         self.state = 'approved'
+        employee = self.env['hr.employee'].search([('id','=',self.nama_karyawan.id)])
+        employee.write({'department_id':self.cabang_baru.id})
+        employee.write({'job_id':self.usulan_jabatan_baru.id})
         
     @api.multi
     def action_reject(self):
         self.state = 'reject'
+        
+    @api.onchange('nama_karyawan')
+    def onchange_cabang_asal(self):
+        if(self.nama_karyawan):
+            employee = self.env['hr.employee'].search([('id','=',self.nama_karyawan.id)])
+            self.cabang_awal = employee.department_id.id
+            self.jabatan_awal = employee.job_id.id
+            
+    @api.model
+    def create(self, vals):
+        employee = self.env['hr.employee'].search([('id','=',vals['nama_karyawan'])])
+        vals['cabang_awal'] = employee.department_id.id
+        vals['jabatan_awal'] = employee.job_id.id
+        return super(hr_promotion, self).create(vals)
+    
+    @api.multi
+    def write(self, vals):
+        employee = self.env['hr.employee'].search([('id','=',vals.get('nama_karyawan'))])
+        if(employee):
+            vals['cabang_awal'] = employee.department_id.id
+            vals['jabatan_awal'] = employee.job_id.id
+        return super(hr_promotion, self).write(vals)
