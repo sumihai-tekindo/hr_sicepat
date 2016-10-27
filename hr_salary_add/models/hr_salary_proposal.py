9ea536c1af67ea47d12e73d719ef97da854c7c81
Pambudi Satria <udi_blackf@yahoo.com>
2016-10-27 17:06:24 +0700

    kenaikan gaji

--- hr_salary_add/models/hr_salary_proposal.py
+++ hr_salary_add/models/hr_salary_proposal.py
@@ -4,24 +4,27 @@ import openerp.addons.decimal_precision as dp
 class hr_salary_proposal(models.Model):
     _name = "hr_salary_proposal"
     
-    name = fields.Char(string="code", default=lambda self: self.env['ir.sequence'].get("salary.proposal"))
-    
-    tanggal = fields.Date(default=lambda self: fields.Date.context_today(self))
-    requestor = fields.Many2one('res.users', string="Requestor", default=lambda self: self.env.user)
+    name = fields.Char(string='Number', readonly=True)
+    tanggal = fields.Date(default=lambda self: fields.Date.context_today(self), readonly=True,
+        states={'draft': [('readonly', False)], 'submit': [('readonly', False)]})
+    requestor = fields.Many2one('res.users', string="Requestor", default=lambda self: self.env.user, readonly=True)
     nama_cabang = fields.Many2one('hr.department', string="Nama Cabang", readonly=True)
-    nama_karyawan = fields.Many2one("hr.employee", string="Nama Karyawan", required=True)
+    nama_karyawan = fields.Many2one("hr.employee", string="Nama Karyawan", required=True, readonly=True,
+        states={'draft': [('readonly', False)], 'submit': [('readonly', False)]})
     jabatan = fields.Many2one('hr.job', string="Jabatan", readonly=True)
-    gaji_semula = fields.Float(digits=dp.get_precision('Payroll'), string="Gaji Awal", required=True)
-    kenaikan_gaji = fields.Float(digits=dp.get_precision('Payroll'), string="Kenaikan Gaji", required=True)
-    gaji_usulan = fields.Float(digits=dp.get_precision('Payroll'), string="Gaji Usulan", compute="compute_gaji_usulan", required=True)
-    alasan = fields.Text()
-    
+    contract_id = fields.Many2one('hr.contract', string='Contract')
+    wage = fields.Float(string='Wage', digits=dp.get_precision('Payroll'))
+    gaji_semula = fields.Float(digits=dp.get_precision('Payroll'), string="Gaji Awal", compute="compute_gaji", readonly=True)
+    kenaikan_gaji = fields.Float(digits=dp.get_precision('Payroll'), string="Kenaikan Gaji", required=True, readonly=True,
+        states={'draft': [('readonly', False)], 'submit': [('readonly', False)]})
+    gaji_usulan = fields.Float(digits=dp.get_precision('Payroll'), string="Gaji Usulan", compute="compute_gaji", readonly=True)
+    alasan = fields.Text(readonly=True, states={'draft': [('readonly', False)], 'submit': [('readonly', False)]})
     state = fields.Selection([
-            ('open','Open'),
+            ('draft','Open'),
             ('submit','Submit'),
             ('reject','Reject'),
             ('approved','Approved'),
-        ], string='Status', default='open')
+        ], string='Status', default='draft', track_visibility='onchange', copy=False,)
     
     @api.multi
     def action_submit(self):
@@ -36,29 +39,42 @@ class hr_salary_proposal(models.Model):
         self.state = 'reject'
         
     @api.one
-    @api.depends('gaji_semula','kenaikan_gaji')
-    def compute_gaji_usulan(self):
-        if((self.gaji_semula!=0) and (self.kenaikan_gaji!=0)):
-            self.gaji_usulan = self.gaji_semula+self.kenaikan_gaji
+    @api.depends('wage','kenaikan_gaji')
+    def compute_gaji(self):
+#         if((self.gaji_semula!=0) and (self.kenaikan_gaji!=0)):
+#             self.gaji_usulan = self.gaji_semula+self.kenaikan_gaji
+        self.gaji_semula = self.wage
+        self.gaji_usulan = self.gaji_semula + self.kenaikan_gaji
             
-    @api.onchange('nama_karyawan')
-    def onchange_cabang(self):
-        if(self.nama_karyawan):
-            employee = self.env['hr.employee'].search([('id','=',self.nama_karyawan.id)])
-            self.nama_cabang = employee.department_id.id
-            self.jabatan = employee.job_id.id
+    @api.onchange('nama_karyawan','tanggal')
+    def onchange_karyawan(self):
+        self.nama_cabang = False
+        self.jabatan = False
+        self.contract_id = False
+        self.wage = 0.0
+        
+        if self.nama_karyawan:
+            self.nama_cabang = self.nama_karyawan.department_id and self.nama_karyawan.department_id.id or False
+            self.jabatan = self.nama_karyawan.job_id and self.nama_karyawan.job_id.id or False
+            contract_ids = self.env['hr.payslip'].get_contract(self.nama_karyawan, self.tanggal, self.tanggal)
+            if contract_ids:
+                contract = self.env['hr.contract'].browse(contract_ids[0])
+                print('contract_id: %s' % contract.id)
+                self.contract_id = contract and contract.id or False
+                self.wage = contract and contract.wage or False
             
     @api.model
     def create(self, vals):
+        vals['name'] = self.env['ir.sequence'].get("salary.proposal")
         employee = self.env['hr.employee'].search([('id','=',vals['nama_karyawan'])])
-        vals['nama_cabang'] = employee.department_id.id
-        vals['jabatan'] = employee.job_id.id
+        vals['nama_cabang'] = employee.department_id and employee.department_id.id or False
+        vals['jabatan'] = employee.job_id and employee.job_id.id or False
         return super(hr_salary_proposal, self).create(vals)
     
     @api.multi
     def write(self, vals):
-        employee = self.env['hr.employee'].search([('id','=',vals.get('nama_karyawan'))])
-        if(employee):
-            vals['nama_cabang'] = employee.department_id.id
-            vals['jabatan'] = employee.job_id.id
+        if vals.get('nama_karyawan'):
+            employee = self.env['hr.employee'].search([('id','=',vals.get('nama_karyawan'))])
+            vals['nama_cabang'] = employee.department_id and employee.department_id.id or False
+            vals['jabatan'] = employee.job_id and employee.job_id.id or False
         return super(hr_salary_proposal, self).write(vals)
