a03f544fef5adf61e92ebaac1ee410aa9ef0673f
timotiuswigianto <timotius@sicepat.com>
2016-10-27 17:03:46 +0700

    update hr employee update hr memorandum

--- hr_salary_add/models/hr_employee.py
+++ hr_salary_add/models/hr_employee.py
@@ -4,17 +4,10 @@ import openerp.addons.decimal_precision as dp
 class hr_employee(models.Model):
     _inherit = 'hr.employee'
     
-    flag = fields.Boolean(compute="compute_flag", string="Status SP")
-    memorandum_ids = fields.One2many("hr_memorandum","nama_karyawan")
+    flag = fields.Boolean(readonly=True, string="Status SP")
+#     memorandum_ids = fields.One2many("hr_memorandum","nama_karyawan")
     referensi = fields.Selection([
             ('jobdb','jobsDB'),
             ('koran','Koran'),
             ('karyawan','karyawan'),
         ], string='Referensi')
-    
-    @api.one
-    @api.depends('memorandum_ids.flag')
-    def compute_flag(self):
-        sp = self.env['hr_memorandum'].search([('nama_karyawan','=',self.id)]) or False
-        if(sp):
-            self.flag = sp.flag
--- hr_salary_add/models/hr_memorandum.py
+++ hr_salary_add/models/hr_memorandum.py
@@ -8,10 +8,10 @@ class hr_memorandum(models.Model):
     tanggal = fields.Date(default=lambda self: fields.Date.context_today(self))
     nama_karyawan = fields.Many2one("hr.employee", string="Nama Karyawan", required=True)
     nama_atasan = fields.Many2one("hr.employee", string="Nama Atasan", required=True)
-    jabatan = fields.Many2one('hr.job', string="Jabatan", required=True)
-    nama_cabang = fields.Many2one('account.analytic.account', string="Nama Cabang", required=True)
+    jabatan = fields.Many2one('hr.job', string="Jabatan", compute="_compute_cabang")
+    nama_cabang = fields.Many2one('hr.department', string="Nama Cabang", compute="_compute_cabang")
     alasan = fields.Text()
-    flag = fields.Boolean()
+    flag = fields.Boolean(string="Status SP")
     
     state = fields.Selection([
             ('open','Open'),
@@ -27,7 +27,39 @@ class hr_memorandum(models.Model):
     @api.multi
     def action_approve(self):
         self.state = 'approved'
+        employee = self.env['hr.employee'].search([('id','=',self.nama_karyawan.id)])
+        employee.write({'flag':self.flag})
         
     @api.multi
     def action_reject(self):
         self.state = 'reject'
+        
+#     @api.onchange('nama_karyawan')
+#     def onchange_cabang_asal(self):
+#         if(self.nama_karyawan):
+#             employee = self.env['hr.employee'].search([('id','=',self.nama_karyawan.id)])
+#             self.nama_cabang = employee.department_id.id
+#             self.jabatan = employee.job_id.id
+            
+    @api.one
+    @api.depends('nama_karyawan')
+    def _compute_cabang(self):
+        employee = self.env['hr.employee'].search([('id','=',self.nama_karyawan.id)])
+        self.nama_cabang = employee.department_id.id
+        self.jabatan = employee.job_id.id
+        self.nama_atasan = employee.parent_id.id
+            
+    @api.model
+    def create(self, vals):
+        employee = self.env['hr.employee'].search([('id','=',vals['nama_karyawan'])])
+        vals['nama_cabang'] = employee.department_id.id
+        vals['jabatan'] = employee.job_id.id
+        return super(hr_memorandum, self).create(vals)
+    
+    @api.multi
+    def write(self, vals):
+        employee = self.env['hr.employee'].search([('id','=',vals.get('nama_karyawan'))])
+        if(employee):
+            vals['nama_cabang'] = employee.department_id.id
+            vals['jabatan'] = employee.job_id.id
+        return super(hr_memorandum, self).write(vals)
