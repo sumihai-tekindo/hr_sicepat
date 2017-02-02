b565b401a7829279572ced191453b25aa7ef5821
Pambudi Satria <udi_blackf@yahoo.com>
2017-02-02 11:17:28 +0700

    [hr_salary_add]: change all method with api in hr.insentif

--- hr_salary_add/models/hr_insentif.py
+++ hr_salary_add/models/hr_insentif.py
@@ -25,6 +25,8 @@ import openerp.addons.decimal_precision as dp
 
 class HRInsentif(models.Model):
     _name = 'hr.insentif'
+    _description= 'HR Insentif'
+    _order = "tanggal desc, id desc"
     
     name = fields.Char(string='Number', readonly=True)
     request_id = fields.Many2one('res.users', string='Requestor', readonly=True,
@@ -58,9 +60,6 @@ class HRInsentif(models.Model):
         vals['name'] = self.env['ir.sequence'].get('hr.insentif')
         return super(HRInsentif, self).create(vals)
 
-# from .hr_salary_structure_amt import CODE2INPUT
-# CODE2INPUT['INSENTIF']='nilai_insentif'
-CODE2INPUT = {'INSENTIF': 'nilai_insentif'}
 
 class HRInsentifLine(models.Model):
     _name = 'hr.insentif.line'
@@ -68,15 +67,14 @@ class HRInsentifLine(models.Model):
 
     insentif_id = fields.Many2one('hr.insentif', string='Insentif')
     employee_id = fields.Many2one('hr.employee', string='Nama Karyawan', required=True)
-    jabatan_id = fields.Many2one('hr.job', string='Jabatan', readonly=True)
+    jabatan_id = fields.Many2one('hr.job', string='Jabatan', compute='_get_employee', store=True, readonly=True)
+    department_id = fields.Many2one('hr.department', string='Nama Cabang', compute='_get_employee', store=True, readonly=True)
     nilai_insentif = fields.Float(digits=dp.get_precision('Payroll'), string='Nilai Insentif', required=True)
     alasan = fields.Text()
     tanggal = fields.Date(related='insentif_id.tanggal', store=True)
-#     department_id = fields.Many2one('hr.department', string='Nama Cabang', related='insentif_id.department_id')
-    department_id = fields.Many2one('hr.department', string='Nama Cabang', readonly=True)
-    state = fields.Selection(related='insentif_id.state', store=True, default='draft')
 
-    def get_insentif_line(self, cr, uid, employee, date_from, date_to, context=None):
+    @api.model
+    def get_insentif_line(self, employee, date_from, date_to):
         """
         @param employee: browse record of employee
         @param date_from: date field
@@ -84,50 +82,17 @@ class HRInsentifLine(models.Model):
         @return: returns the ids of all the salary structure lines for the given employee that need to be considered for the given dates
         """
         clause_1 = ['&',('tanggal', '<=', date_to),('tanggal','>=', date_from)]
-        clause_final = [('employee_id','=',employee.id), ('state','=','approved')] + clause_1
-        insentif_line_ids = self.search(cr, uid, clause_final, order='tanggal desc', context=context)
+        clause_final = [('employee_id','=',employee.id), ('insentif_id.state','=','approved')] + clause_1
+        insentif_line_ids = self.search(clause_final, order='tanggal desc')
         return insentif_line_ids
 
-    def get_amount(self, cr, uid, ids, code, context=None):
-        """
-        @param code: char field
-        @return: returns amount based on code
-        """
-        insentif_line = self.browse(cr, uid, ids, context)
-        return insentif_line[0][CODE2INPUT.get(code)]
-
-    def get_condition(self, cr, uid, code, context=None):
-        """
-        @param code: char field
-        @return: returns True or False
-        """
-        if code:
-            if code in CODE2INPUT:
-                return True
-        return False
-    
-    @api.onchange('employee_id')
-    def onchange_cabang_asal(self):
-        if(self.employee_id):
-            employee = self.env['hr.employee'].search([('id','=',self.employee_id.id)])
-            self.department_id = employee.department_id.id
-            self.jabatan_id = employee.job_id.id
+    @api.one
+    @api.depends('employee_id')
+    def _get_employee(self):
+        self.jabatan_id = self.employee_id.job_id.id
+        self.department_id = self.employee_id.department_id.id
             
-    @api.model
-    def create(self, vals):
-        employee = self.env['hr.employee'].search([('id','=',vals['employee_id'])])
-        vals['department_id'] = employee.department_id.id
-        vals['jabatan_id'] = employee.job_id.id
-        return super(HRInsentifLine, self).create(vals)
-    
-    @api.multi
-    def write(self, vals):
-        employee = self.env['hr.employee'].search([('id','=',vals.get('employee_id'))])
-        if(employee):
-            vals['department_id'] = employee.department_id.id
-            vals['jabatan_id'] = employee.job_id.id
-        return super(HRInsentifLine, self).write(vals)
-    
+
 class HRPayslip(models.Model):
     _inherit = 'hr.payslip'
 
@@ -142,9 +107,9 @@ class HRPayslip(models.Model):
         employee = employee_obj.browse(cr, uid, employee_id, context=context)
         
         for result in res:
-            if insentif_line.get_condition(cr, uid, result.get('code'), context=context):
+            if result.get('code') == 'INSENTIF':
                 struct_line_ids = insentif_line.get_insentif_line(cr, uid, employee, date_from, date_to, context=context)
                 if struct_line_ids:
-                    result['amount'] = insentif_line.get_amount(cr, uid, struct_line_ids, result['code'], context=context)
+                    result['amount'] = struct_line_ids[0].nilai_insentif
 
         return res
