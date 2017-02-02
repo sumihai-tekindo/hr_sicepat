69fb85414948a5fda738aa89cf45995878bec4f0
Pambudi Satria <udi_blackf@yahoo.com>
2017-02-02 11:19:24 +0700

    [hr_salary_add]: change all method to api in hr.overtime

--- hr_salary_add/models/hr_overtime.py
+++ hr_salary_add/models/hr_overtime.py
@@ -21,6 +21,7 @@
 ##############################################################################
 
 from openerp import api, fields, models
+from openerp.exceptions import Warning
 from openerp.osv import osv
 from openerp.tools.translate import _
 import openerp.addons.decimal_precision as dp
@@ -33,8 +34,6 @@ class HROvertime(models.Model):
         default=lambda self: self.env.user)
     tanggal = fields.Date(default=lambda self: fields.Date.context_today(self), readonly=True,
         states={'draft': [('readonly', False)], 'submit': [('readonly', False)]})
-#     department_id = fields.Many2one('hr.department', string='Nama Cabang', required=True, readonly=True,
-#         states={'draft': [('readonly', False)], 'submit': [('readonly', False)]})
     state = fields.Selection([
         ('draft','Open'),
         ('submit','Submit'),
@@ -56,33 +55,22 @@ class HROvertime(models.Model):
 
     @api.multi
     def action_approve(self):
-        status = False
         for line in self.overtime_line:
-            if(line.state=='draft'):
-                status=True
-        if(status==False):
-            self.state = 'approved'
-        else:
-            raise osv.except_osv(_('Gagal Di Proses'), _('silahkan lakukan Approve atau Reject setiap lemburan'))
+            if line.state not in ('confirmed', 'cancel'):
+                raise Warning(_("Silahkan lakukan Approve atau Reject setiap lemburan."))
+        self.state = 'approved'
         
     @api.multi
     def action_reject(self):
-        status = False
         for line in self.overtime_line:
-            if(line.state=='draft'):
-                status=True
-        if(status==False):
-            self.state = 'reject'
-        else:
-            raise osv.except_osv(_('Gagal Di Proses'), _('silahkan lakukan Approve atau Reject setiap lemburan'))
+            if line.state not in ('confirmed', 'cancel'):
+                raise Warning(_("Silahkan lakukan Approve atau Reject setiap lemburan."))
+        self.state = 'reject'
         
     @api.multi
     def action_proses(self):
         self.state = 'proses'
 
-# from .hr_salary_structure_amt import CODE2INPUT
-# CODE2INPUT['OVERTIME']='nilai'
-CODE2INPUT = {'OVERTIME': 'nilai'}
 
 class HROvertimeLine(models.Model):
     _name = 'hr.overtime.line'
@@ -90,7 +78,8 @@ class HROvertimeLine(models.Model):
     
     overtime_id = fields.Many2one('hr.overtime', string='Overtime')
     employee_id = fields.Many2one('hr.employee', string='Nama Karyawan', required=True)
-    jabatan_id = fields.Many2one('hr.job', string='Jabatan', related='employee_id.job_id', readonly=True)
+    jabatan_id = fields.Many2one('hr.job', string='Jabatan', compute='_get_employee', store=True, readonly=True)
+    department_id = fields.Many2one('hr.department', string='Nama Cabang', compute='_get_employee', store=True, readonly=True)
     nilai = fields.Float(digits=dp.get_precision('Payroll'), string='Nilai Lemburan', required=True)
     alasan = fields.Text()
     state = fields.Selection([
@@ -99,8 +88,6 @@ class HROvertimeLine(models.Model):
         ('cancel','Cancel'),
         ], string='Status', default='draft')
     tanggal = fields.Date(related='overtime_id.tanggal', store=True)
-#     department_id = fields.Many2one('hr.department', string='Nama Cabang', related='overtime_id.department_id')
-    department_id = fields.Many2one('hr.department', string='Nama Cabang', readonly=True)
     overtime_state = fields.Selection(related='overtime_id.state', store=True, default='draft')
     
     @api.multi
@@ -111,7 +98,14 @@ class HROvertimeLine(models.Model):
     def action_cancel(self):
         self.state = 'cancel'
 
-    def get_overtime_line(self, cr, uid, employee, date_from, date_to, context=None):
+    @api.one
+    @api.depends('employee_id')
+    def _get_employee(self):
+        self.jabatan_id = self.employee_id.job_id.id
+        self.department_id = self.employee_id.department_id.id
+            
+    @api.model
+    def get_overtime_line(self, employee, date_from, date_to):
         """
         @param employee: browse record of employee
         @param date_from: date field
@@ -119,50 +113,11 @@ class HROvertimeLine(models.Model):
         @return: returns the ids of all the salary structure lines for the given employee that need to be considered for the given dates
         """
         clause_1 = ['&',('tanggal', '<=', date_to),('tanggal','>=', date_from)]
-        clause_final = [('employee_id','=',employee.id), ('state','=','confirmed'), ('overtime_state','=','proses')] + clause_1
-        overtime_line_ids = self.search(cr, uid, clause_final, order='tanggal desc', context=context)
+        clause_final = [('employee_id','=',employee.id), ('state','=','confirmed'), ('overtime_id.state','=','proses')] + clause_1
+        overtime_line_ids = self.search(clause_final, order='tanggal desc')
         return overtime_line_ids
 
-    def get_amount(self, cr, uid, ids, code, context=None):
-        """
-        @param code: char field
-        @return: returns amount based on code
-        """
-        overtime_line = self.browse(cr, uid, ids, context)
-        return overtime_line[0][CODE2INPUT.get(code)]
 
-    def get_condition(self, cr, uid, code, context=None):
-        """
-        @param code: char field
-        @return: returns True or False
-        """
-        if code:
-            if code in CODE2INPUT:
-                return True
-        return False
-    
-    @api.onchange('employee_id')
-    def onchange_cabang_asal(self):
-        if(self.employee_id):
-            employee = self.env['hr.employee'].search([('id','=',self.employee_id.id)])
-            self.department_id = employee.department_id.id
-            self.jabatan_id = employee.job_id.id
-            
-    @api.model
-    def create(self, vals):
-        employee = self.env['hr.employee'].search([('id','=',vals['employee_id'])])
-        vals['department_id'] = employee.department_id.id
-        vals['jabatan_id'] = employee.job_id.id
-        return super(HROvertimeLine, self).create(vals)
-    
-    @api.multi
-    def write(self, vals):
-        employee = self.env['hr.employee'].search([('id','=',vals.get('employee_id'))])
-        if(employee):
-            vals['department_id'] = employee.department_id.id
-            vals['jabatan_id'] = employee.job_id.id
-        return super(HROvertimeLine, self).write(vals)
-    
 class HRPayslip(models.Model):
     _inherit = 'hr.payslip'
 
@@ -177,9 +132,9 @@ class HRPayslip(models.Model):
         employee = employee_obj.browse(cr, uid, employee_id, context=context)
         
         for result in res:
-            if overtime_line.get_condition(cr, uid, result.get('code'), context=context):
+            if result.get('code') == 'OVERTIME':
                 struct_line_ids = overtime_line.get_overtime_line(cr, uid, employee, date_from, date_to, context=context)
                 if struct_line_ids:
-                    result['amount'] = overtime_line.get_amount(cr, uid, struct_line_ids, result['code'], context=context)
+                    result['amount'] = struct_line_ids[0].nilai
 
         return res
