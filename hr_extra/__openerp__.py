b35d7f7d17974b297ee5b6e132d3503ed9af9602
andreanw96 <andreanw96@gmail.com>
2017-10-05 17:07:34 +0700

    modul hr_extra, masalah cron hrd

---
+++ hr_extra/__openerp__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'HR Extra',
+    'version': '8.0.1.0.0',
+    'license': 'AGPL-3',
+    'category': 'Generic Modules/HR Extra',
+    'author': 'Andrean Wijaya',
+    'website': '-',
+    'depends': ['hr'],
+    'data': [
+        'wizard/cron_hr.xml',
+        'wizard/template_email_hr.xml',
+    ],
+    'installable': True,
+}
