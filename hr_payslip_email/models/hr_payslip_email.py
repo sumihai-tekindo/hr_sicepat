6346715a60af71eaa33347546da664f03fca0a81
adeanugerah89 <ade_anugerahtb@yahoo.com>
2018-09-06 11:25:38 +0700

    simplyfied context

--- hr_payslip_email/models/hr_payslip_email.py
+++ hr_payslip_email/models/hr_payslip_email.py
@@ -1,10 +1,7 @@
-import cStringIO
-import base64
 from openerp import models, fields, api, exceptions, _, tools
 from openerp.exceptions import except_orm, Warning, RedirectWarning
 from openerp import SUPERUSER_ID
 from datetime import datetime
-from PyPDF2 import PdfFileWriter, PdfFileReader
 
 class EmailPayslipTest(models.TransientModel):
 	_name = 'hr.payslip.email'
@@ -17,33 +14,37 @@ class EmailPayslipTest(models.TransientModel):
 	@api.multi
 	def send_via_email(self):
 		self.ensure_one()
-		
+
+		ctx = dict(self._context)
+		template = self.env.ref('hr_payslip_email.email_payslip_template')
+		ctx.update({
+			'default_model': 'hr.payslip',
+			'active_model': 'hr.payslip',
+			'model': 'hr.payslip',
+			'default_use_template': bool(template),
+			'default_template_id': template and template.id or False,
+		})
+
 		if self.payslip_ids and len(self.payslip_ids) == 1:
 			if not self.payslip_ids[0].employee_id.work_email:
 				raise except_orm(_('No Email Defined!'),_("You must define email address for this employee!"))
 			else:
-				template = self.env.ref('hr_payslip_email.email_payslip_template')
 				compose_form = self.env.ref('mail.email_compose_message_wizard_form', False)
 				birthday = self.payslip_ids[0].employee_id.birthday
 				# set default password if birthday null
-				password = self.payslip_ids[0].employee_id.nik
+				payslip_password = self.payslip_ids[0].employee_id.nik
 				if birthday:
 					# 2 digit from each of the employee's birthday number
-					password = ''.join(res[-2:] for res in birthday.split('-')[::-1])
+					payslip_password = ''.join(res[-2:] for res in birthday.split('-')[::-1])
 
-				ctx = dict(
-					default_model = 'hr.payslip',
-					active_model = 'hr.payslip',
-					model = 'hr.payslip',
-					default_use_template = bool(template),
-					default_template_id = template and template.id or False,
-					composition_mode = 'comment',
-					active_ids = [self.payslip_ids[0].id],
-					active_id = self.payslip_ids[0].id,
-					default_res_id = self.payslip_ids[0].id,
-					res_id = self.payslip_ids[0].id,
-					payslip_password = password
-				)
+				ctx.update({
+					'composition_mode': 'comment',
+					'active_ids': [self.payslip_ids[0].id],
+					'active_id': self.payslip_ids[0].id,
+					'default_res_id': self.payslip_ids[0].id,
+					'res_id': self.payslip_ids[0].id,
+					'set_password': payslip_password
+				})
 
 				return {
 					'name': _('Compose Email for Payslip'),
@@ -60,7 +61,6 @@ class EmailPayslipTest(models.TransientModel):
 		else:
 			mail_message = self.env['mail.message']
 			user = self.env['res.users'].browse(self._uid)
-			import base64
 			for payslip in self.payslip_ids:
 				if not payslip.employee_id.work_email:
 					mail_message.sudo().create({
@@ -72,7 +72,6 @@ class EmailPayslipTest(models.TransientModel):
 							'body': 'Email untuk karyawan %s (%s) tidak terdefinisi' % (payslip.employee_id.name, payslip.employee_id.nik),
 						})
 				else:	
-					template = self.env.ref('hr_payslip_email.email_payslip_template')
 					birthday = payslip.employee_id.birthday
 					# set default password if birthday null
 					password = payslip.employee_id.nik
@@ -80,23 +79,18 @@ class EmailPayslipTest(models.TransientModel):
 						# 2 digit from each of the employee's birthday number
 						password = ''.join(res[-2:] for res in birthday.split('-')[::-1])
 
-					ctx = dict(
-						default_model = 'hr.payslip',
-						active_model = 'hr.payslip',
-						model = 'hr.payslip',
-						default_use_template = bool(template),
-						default_template_id = template and template.id or False,
-						active_ids = [payslip.id],
-						active_id = payslip.id,
-						default_res_id = payslip.id,
-						res_id = payslip.id,
-						payslip_password = password
-					)
+					ctx.update({
+						'active_ids': [payslip.id],
+						'active_id': payslip.id,
+						'default_res_id': payslip.id,
+						'res_id': payslip.id,
+						'set_password': payslip_password
+					})
 
 					template.with_context(ctx).send_mail(payslip.id, force_send=True, raise_exception=True)
 
 		return True
-					
+
 class HRPayslip(models.Model):
 	_name = 'hr.payslip'
 	_inherit = ['hr.payslip','mail.thread']
@@ -106,33 +100,3 @@ class ResCompany(models.Model):
 
 	hr_line_id = fields.Char(string="Line Id")
 	hr_phone_contact = fields.Char(string="Contact Number")
-
-class Report(models.Model):
-	_inherit = 'report'
-
-	def get_pdf(self, cr, uid, ids, report_name, html=None, data=None, context=None):
-		result = super(Report, self).get_pdf(cr, uid, ids, report_name, html=html, data=data, context=context)
-
-		if context.get('payslip_password',False):
-			password = context.get('payslip_password')
-
-			unencrypted_file = cStringIO.StringIO()
-			unencrypted_file.write(result)
-
-			# make a copy of the PDF file, and encrypt it
-			pdf_file_reader = PdfFileReader(unencrypted_file)
-			pdf_file_writer = PdfFileWriter()
-			for page in pdf_file_reader.pages:
-				pdf_file_writer.addPage(page)
-			pdf_file_writer.encrypt(password)
-
-			# write the encrypted payslip PDF content into a "memory file"
-			encrypted_file = cStringIO.StringIO()
-			pdf_file_writer.write(encrypted_file)
-			unencrypted_file.close()
-
-			result = encrypted_file.getvalue()
-
-		return result
-
-	
